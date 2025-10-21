4abc303a4f32f807f683b6862b6f55b14934225e
TheLiquidIvy <theliquidivy@gmail.com>
2025-10-21 05:12:51 +0200

    Add token authentication middleware

---
+++ server/middleware/authenticateToken.js
@@ -0,0 +1,17 @@
+// server/middleware/authenticateToken.js
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Token missing' });
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Invalid token' });
+    req.userId = user.id;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
